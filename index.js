77438cc98945855ec78bbdd40f7c641f5f94ad8f
Nestor Huallpa <huallpa.nestor@gmail.com>
2018-05-11 00:26:58 -0300

    cambio puerto

--- index.js
+++ index.js
@@ -63,4 +63,4 @@ app.get('/webhook', (req, res) => {
 });
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 443, () => console.log('webhook is listening'));
